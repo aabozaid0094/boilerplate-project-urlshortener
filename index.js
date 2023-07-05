b38464d4d0c16254a638f009a417b4fe924ff444
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-07-05 21:13:10 +0300

    Use express json and urlencoded methods to read post body

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
