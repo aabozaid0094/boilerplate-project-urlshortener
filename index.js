fed16fe30ecea30b9c989b3f49695c9f43604a8f
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-07-05 21:19:28 +0300

    Add shorten url api post&get endpoints

--- index.js
+++ index.js
@@ -19,8 +19,38 @@ app.get("/", function (req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+    try {
+        const { hostname } = new URL(req.body.url);
+        dns.lookup(hostname, (err, address, family) => {
+            if (!err) {
+                urls.add(req.body.url);
+                res.json({
+                    original_url: req.body.url,
+                    short_url: Array.from(urls).indexOf(req.body.url) + 1,
+                });
+            } else {
+                res.json({ error: "Invalid URL" });
+            }
+        });
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+    const intIndex = parseInt(req.params.id) - 1;
+    if (intIndex + 1 > urls.size || !Array.from(urls)[intIndex]) {
+        res.json({ error: "No short URL found for the given input" });
+    } else {
+        const externalUrl = Array.from(urls)[intIndex];
+        try {
+            const externalUrlObject = new URL(externalUrl);
+            res.redirect(externalUrl);
+        } catch (error) {
+            res.json({ error: error.message });
+        }
+    }
 });
 
 app.listen(port, function () {
